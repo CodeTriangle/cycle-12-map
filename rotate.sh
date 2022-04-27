db02920ed1c042ec0c3396bdc718335744326ba4
Richard Snider <lighthousemaniac@gmail.com>
2022-04-27 21:29:08 +0000

    rotation

--- rotate.sh
+++ rotate.sh
@@ -3,8 +3,8 @@
 NAME="$1"
 DEGREES="${2-90}"
 IMGDIR="docs/img"
-NORMALIMG="$IMGDIR/$NAME.png"
-THUMBIMG="$IMGDIR/$NAME-thumb.png"
+NORMALIMG="$IMGDIR/$NAME.webp"
+THUMBIMG="$IMGDIR/$NAME-thumb.webp"
 
 convert "$NORMALIMG" -rotate "$DEGREES" "$NORMALIMG"
 convert "$THUMBIMG" -rotate "$DEGREES" "$THUMBIMG"
